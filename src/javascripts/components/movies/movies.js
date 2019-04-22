fdc310433487d4e5d7e160b4b1d76a66c0c7f3d6
Greg Stephen <GregAStephen@gmail.com>
2019-04-22 18:54:35 -0500

    movie cards

--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -7,7 +7,19 @@ let movies = [];
 const domStringBuilder = () => {
   let domString = '';
   movies.forEach((movie) => {
-    domString += `<h3>${movie.name}</h3>`;
+    domString += `<div id="${movie.id}" class="movie card col-5 mt-5">`;
+    domString += `<div class="card-header">${movie.name}</div>`;
+    domString += '<div class="card-body">';
+    domString += `<h5 class="card-title">Genre: ${movie.genre}</h5>`;
+    domString += '<p class="card-text">';
+    domString += '<ul>';
+    domString += `<li>Release Date: ${movie.releaseDate}</li>`;
+    domString += `<li>Description: ${movie.description}</li>`;
+    domString += `<li>How Many Locations: ${movie.locations.length}</li>`;
+    domString += '</ul>';
+    domString += '</p>';
+    domString += '</div>';
+    domString += '</div>';
   });
   util.printToDom('movies', domString);
 };
