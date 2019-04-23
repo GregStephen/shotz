8dee4ed678bce0933044d7649bfd77b5712ee20c
Greg Stephen <GregAStephen@gmail.com>
2019-04-22 19:27:09 -0500

    color coded baby

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -4,11 +4,33 @@ import locationsData from '../../helpers/data/locationsData';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+      break;
+  }
+  return selectedClass;
+};
+
 const domStringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
     domString += `<div id="${location.id}" class="movie card col-2 mt-5">`;
-    domString += `<div class="card-header">${location.name}</div>`;
+    domString += `<div class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</div>`;
     domString += `<img class="card-img-top" src="${location.imageUrl}" alt="Card image cap"></img>`;
     domString += '<div class="card-body">';
     domString += `<h5 class="card-title">Location: ${location.address}</h5>`;
